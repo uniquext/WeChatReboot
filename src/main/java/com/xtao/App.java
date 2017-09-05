a9e6c3344d958c659ee0393ecb38f6895a156027
xtao <penghaitao918@163.com>
2017-09-05 19:25:03 +0800

    first commit

---
+++ src/main/java/com/xtao/App.java
@@ -0,0 +1,13 @@
+package com.xtao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
